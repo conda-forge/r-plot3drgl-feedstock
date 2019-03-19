130b3873d2086a8a02db24c2163c8f27ae72f24d
Daniel Bast <2790401+dbast@users.noreply.github.com>
2019-03-19 22:58:46 +0100

    Add workaround to make libGL available during build + test
    Users shouldn't need to set LD_LIBRARY_PATH, libGL will be in some default
    location on their system. This trick should only be needed during CI
    testing since it's in a non-standard location.
    
    How it is resloved on user side:
    ~/miniconda3/envs/r-rgl$ ldd -r lib/R/library/rgl/libs/rgl.so
    	linux-vdso.so.1 (0x00007ffeb39cf000)
    	libGLU.so.1 => /home/$USER/miniconda3/envs/r-rgl/lib/R/library/rgl/libs/../../../../libGLU.so.1 (0x00007f4760ea6000)
    	libGL.so.1 => /usr/lib/x86_64-linux-gnu/libGL.so.1 (0x00007f4760ac9000)
            ...

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
+  if [[ ${HOST} =~ .*linux.* ]]; then
+    X11_CONFIGURE_ARGS="--x-includes=${PREFIX}/include --x-libraries=${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64"
+    export LD_LIBRARY_PATH=${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64
+  fi
   export DISABLE_AUTOBREW=1
   $R CMD INSTALL --build .
 else
